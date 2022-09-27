7793757936d81827dd836b1818016071d1d9e78d
ambuj <ambujs89@github.com>
2022-09-27 14:23:14 +0530

    create serve-static middleware for node js

---
+++ src/serve-static.ts
@@ -0,0 +1,43 @@
+import { existsSync, readFileSync } from 'fs'
+import type { MiddlewareHandler } from 'hono/src/hono'
+import { getFilePath } from 'hono/utils/filepath'
+import { getMimeType } from 'hono/utils/mime'
+
+export type ServeStaticOptions = {
+  root?: string
+  path?: string
+  defualtDocument?: string
+}
+
+export const serveStatic = (options: ServeStaticOptions = { root: '' }): MiddlewareHandler => {
+  return async (c, next): Promise<Response | undefined> => {
+    // Do nothing if Response is already set
+    if (c.res && c.finalized) {
+      await next()
+    }
+    const url = new URL(c.req.url)
+
+    let path = getFilePath({
+      filename: options.path ?? url.pathname,
+      root: options.root,
+      defaultDocument: options.defualtDocument ?? 'index.html',
+    })
+    path = `./${path}`
+
+    if (existsSync(path)) {
+      const content = readFileSync(path)
+      if (content) {
+        const mimeType = getMimeType(path)
+        if (mimeType) {
+          c.res.headers.set('Content-Type', mimeType)
+        }
+        // Return Response object
+        return c.body(content)
+      }
+    }
+
+    console.warn(`Static file: ${path} is not found`)
+    await next()
+    return
+  }
+}
