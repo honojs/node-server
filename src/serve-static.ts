ec09fd9de7cebf9cef1171787476e9e6d2b4fdae
Benjamin <45743294+Hoodgail@users.noreply.github.com>
2023-01-20 21:11:35 -0500

    Update serve-static.ts
    Many fixes

--- src/serve-static.ts
+++ src/serve-static.ts
@@ -1,44 +1,105 @@
-import type { Next } from 'hono'
-import { Context } from 'hono'
-import { existsSync, readFileSync } from 'fs'
+import type { Next, Context, Handler } from 'hono'
+import { createReadStream, existsSync, lstatSync } from 'fs'
 import { getFilePath } from 'hono/utils/filepath'
 import { getMimeType } from 'hono/utils/mime'
 
 export type ServeStaticOptions = {
-  root?: string
-  path?: string
-  index?: string // default is 'index.html'
-}
+     root?: string
+     path?: string
+     index?: string // default is 'index.html'
+};
 
-export const serveStatic = (options: ServeStaticOptions = { root: '' }) => {
-  return async (c: Context, next: Next): Promise<Response | undefined> => {
-    // Do nothing if Response is already set
-    if (c.finalized) {
-      await next()
-    }
-    const url = new URL(c.req.url)
+export const serveStatic = (options: ServeStaticOptions = { root: '' }): Handler => {
 
-    let path = getFilePath({
-      filename: options.path ?? url.pathname,
-      root: options.root,
-      defaultDocument: options.index ?? 'index.html',
-    })
-    path = `./${path}`
+     return async (c: Context, next: Next) => {
 
-    if (existsSync(path)) {
-      const content = readFileSync(path)
-      if (content) {
-        const mimeType = getMimeType(path)
-        if (mimeType) {
-          c.header('Content-Type', mimeType)
-        }
-        // Return Response object
-        return c.body(content)
-      }
-    }
+          // Do nothing if Response is already set
+          if (c.finalized) return next()
 
-    console.warn(`Static file: ${path} is not found`)
-    await next()
-    return
-  }
+          const url = new URL(c.req.url);
+
+          let path = getFilePath({
+               filename: options.path ?? url.pathname,
+               root: options.root,
+               defaultDocument: options.index ?? 'index.html',
+          })
+
+          path = `./${path}`;
+
+          if (existsSync(path)) {
+
+               const mimeType = getMimeType(path)
+
+               const stat = lstatSync(path);
+
+               if (mimeType) {
+
+                    c.header('Content-Type', mimeType)
+               }
+
+               c.header('Accept-Ranges', 'bytes');
+
+               c.header("Date", stat.birthtime.toUTCString())
+
+               if (c.req.method == 'HEAD' || c.req.method == 'OPTIONS') {
+
+                    c.header('Content-Length', stat.size.toString());
+
+                    c.status(200);
+
+                    return c.body(null);
+               }
+
+               const range = c.req.headers.get('range');
+
+               let start = 0;
+               let end = stat.size - 1;
+
+               if (range) {
+                    const parts = range.replace(/bytes=/, "").split("-");
+                    start = parseInt(parts[0], 10);
+                    end = parts[1] ? parseInt(parts[1], 10) : end;
+               }
+
+               const chunksize = (end - start) + 1;
+
+               const stream = createReadStream(path, { start, end });
+
+               c.header('Content-Length', chunksize.toString());
+
+               c.header('Content-Range', `bytes ${start}-${end}/${stat.size}`);
+
+               c.status(206);
+
+               const body = new ReadableStream({
+
+                    start(controller) {
+
+                         stream.on('data', (chunk) => {
+
+                              controller.enqueue(chunk);
+
+                         });
+
+                         stream.on('end', () => {
+
+                              controller.close();
+
+                         });
+
+                    },
+
+                    cancel() {
+
+                         stream.destroy();
+
+                    },
+
+               });
+
+               return c.body(body);
+          }
+
+          return next();
+     }
 }
