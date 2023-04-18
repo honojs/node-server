796b19c5e5068f7e67001a231d504cfb760206a2
唐烨 <tangye@xinpianchang.com>
2023-04-18 13:03:55 +0800

    fix: add TransformStream to global and re-implement stream pipe

--- src/stream.ts
+++ src/stream.ts
@@ -1,27 +1,67 @@
-import type { Writable } from 'node:stream'
+import { Writable, Readable } from 'node:stream'
+import { pipeline } from 'node:stream'
+import { promisify } from 'node:util'
 
-export async function writeReadableStreamToWritable(stream: ReadableStream, writable: Writable) {
+const pipelinePromise = promisify(pipeline)
+
+/** pipeline will assure the backpressure and reduce huge memory usage */
+export async function writeReadableStreamToWritable(
+  stream: ReadableStream,
+  writable: Writable
+) {
+  const readable = webReadableStreamToNodeReadable(stream)
+  return pipelinePromise(readable, writable)
+}
+
+/** This implementation use nodejs Readable::fromWeb as references */
+export function webReadableStreamToNodeReadable(
+  stream: ReadableStream
+): Readable {
   const reader = stream.getReader()
+  let closed = false
 
-  function onClose() {
-    reader.cancel(new Error('Response writer closed'))
-  }
-
-  writable.once('close', onClose)
-
-  try {
-    while (true) {
-      const { done, value } = await reader.read()
-
-      if (done) {
-        writable.end()
-        return
+  const readable = new Readable({
+    read() {
+      reader
+        .read()
+        .then(({ done, value }) => {
+          if (done) {
+            this.push(null)
+          } else {
+            this.push(value)
+          }
+        })
+        .catch(e => {
+          readable.destroy(e)
+        })
+    },
+    destroy(error, callback) {
+      const done = () => {
+        try {
+          callback(error)
+        } catch (err: unknown) {
+          process.nextTick(() => {
+            throw err
+          })
+        }
       }
 
-      writable.write(value)
+      if (!closed) {
+        reader.cancel(error).then(done, done)
+        return
+      }
+      done()
+    },
+  })
+
+  reader.closed.then(
+    () => {
+      closed = true
+    },
+    error => {
+      readable.destroy(error)
     }
-  } finally {
-    writable.off('close', onClose)
-    reader.releaseLock()
-  }
+  )
+
+  return readable
 }
