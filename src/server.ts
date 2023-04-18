8052b2f5b25edb6dd48f6fa8b9b84b5d4fee6245
Ibrahim Odev <ibrahimodev42@gmail.com>
2023-04-18 20:27:42 +0300

    add listeningListener serve function

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { createServer, Server } from 'node:http'
 import { installGlobals } from './globals'
 import { Options } from './types'
 import { getRequestListener } from './listener'
+import type { AddressInfo } from 'node:net'
 
 installGlobals()
 
@@ -13,8 +14,12 @@ export const createAdaptorServer = (options: Options): Server => {
   return server
 }
 
-export const serve = (options: Options): Server => {
-  const server = createAdaptorServer(options)
-  server.listen(options.port || 3000, options.hostname || '0.0.0.0')
+export const serve = (options: Options, listeningListener?:(port: number) => void): Server => {
+  const server = createAdaptorServer(options);
+  const serverInfo = server.address() as AddressInfo;
+  const port = serverInfo?.port ?? options.port;
+  server.listen(options.port || 3000, options.hostname || '0.0.0.0', () => {
+    listeningListener && listeningListener(port);
+  })
   return server
 }
