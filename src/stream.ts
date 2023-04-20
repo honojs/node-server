30ad45e367fb5c623941b1a3743feb3643275086
tangye <447084+tangye1234@users.noreply.github.com>
2023-04-20 17:46:07 +0800

    fix: add TransformStream to global and re-implement stream pipe (#34)
    * fix: add TransformStream to global and re-implement stream pipe
    
    * chore: enhance the request body backpressure
    
    * feat: add stream support and fix some bugs
    
    ---------
    
    Co-authored-by: 唐烨 <tangye@xinpianchang.com>

--- src/stream.ts
+++ src/stream.ts
@@ -1,27 +1,128 @@
-import type { Writable } from 'node:stream'
+import { Writable, Readable, pipeline, finished } from 'node:stream'
+import { promisify } from 'node:util'
 
-export async function writeReadableStreamToWritable(stream: ReadableStream, writable: Writable) {
+const pipelinePromise = promisify(pipeline)
+
+/** pipeline will assure the backpressure and reduce huge memory usage */
+export async function writeReadableStreamToWritable(
+  stream: ReadableStream,
+  writable: Writable
+) {
+  const readable = webReadableStreamToNodeReadable(stream)
+  return pipelinePromise(readable, writable)
+}
+
+/** This implementation use nodejs Readable::fromWeb as references */
+export function webReadableStreamToNodeReadable(
+  stream: ReadableStream
+): Readable {
   const reader = stream.getReader()
+  let closed = false
 
-  function onClose() {
-    reader.cancel(new Error('Response writer closed'))
-  }
-
-  writable.once('close', onClose)
-
-  try {
-    while (true) {
-      const { done, value } = await reader.read()
-
-      if (done) {
-        writable.end()
-        return
+  const readable = new Readable({
+    read() {
+      reader
+        .read()
+        .then(({ done, value }) => {
+          if (done) {
+            this.push(null)
+          } else {
+            this.push(value)
+          }
+        })
+        .catch(e => {
+          readable.destroy(e)
+        })
+    },
+    destroy(error, callback) {
+      const done = () => {
+        try {
+          callback(error)
+        } catch (err: unknown) {
+          process.nextTick(() => {
+            throw err
+          })
+        }
       }
 
-      writable.write(value)
+      if (!closed) {
+        reader.cancel(error).then(done, done)
+        return
+      }
+      done()
+    },
+  })
+
+  reader.closed.then(
+    () => {
+      closed = true
+    },
+    error => {
+      readable.destroy(error)
     }
-  } finally {
-    writable.off('close', onClose)
-    reader.releaseLock()
+  )
+
+  return readable
+}
+
+export function nodeReadableToWebReadableStream(readable: Readable) {
+  if (readable.destroyed) {
+    const stream = new ReadableStream<Uint8Array>()
+    stream.cancel()
+    return stream
   }
+
+  const highWaterMark = readable.readableHighWaterMark
+  const strategy = { highWaterMark }
+
+  let controller: ReadableStreamDefaultController<Uint8Array>
+
+  const onData = (chunk: Buffer | Uint8Array) => {
+    // Copy the Buffer to detach it from the pool.
+    if (Buffer.isBuffer(chunk)) {
+      chunk = new Uint8Array(chunk)
+    }
+    controller.enqueue(chunk)
+    if (controller.desiredSize !== null && controller.desiredSize <= 0) {
+      readable.pause()
+    }
+  }
+
+  readable.pause()
+
+  const cleanup = finished(readable, error => {
+    if (error?.code === 'ERR_STREAM_PREMATURE_CLOSE') {
+      const err = new Error(undefined, { cause: error })
+      Object.defineProperty(err, 'name', 'AbortError')
+      error = err
+    }
+
+    cleanup()
+
+    // This is a protection against non-standard, legacy streams
+    // that happen to emit an error event again after finished is called.
+    readable.on('error', () => {})
+    if (error) {
+      return controller.error(error)
+    }
+
+    controller.close()
+  })
+
+  readable.on('data', onData)
+
+  return new ReadableStream<Uint8Array>(
+    {
+      start(c) {
+        controller = c
+      },
+      pull() {
+        readable.resume()
+      },
+      cancel(reason) {
+        readable.destroy(reason)
+      },
+    },
+    strategy
+  )
 }
