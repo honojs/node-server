d1e514e0c2beadd4f5052bf996b048edfe55f640
İbrahim Ödev <74017534+ibodev1@users.noreply.github.com>
2023-04-25 13:32:14 +0300

    feat: update server port info line add initial port varible

--- src/server.ts
+++ src/server.ts
@@ -14,11 +14,12 @@ export const createAdaptorServer = (options: Options): Server => {
   return server
 }
 
-export const serve = (options: Options, listeningListener?:(port: number) => void): Server => {
+export const serve = (options: Options, listeningListener?: (port: number) => void): Server => {
   const server = createAdaptorServer(options);
-  const serverInfo = server.address() as AddressInfo;
-  const port = serverInfo?.port ?? options.port;
-  server.listen(options.port || 3000, options.hostname || '0.0.0.0', () => {
+  const initialPort = options?.port ?? 3000;
+  server.listen(initialPort, options.hostname || '0.0.0.0', () => {
+    const serverInfo = server.address() as AddressInfo;
+    const port = serverInfo?.port ?? initialPort;
     listeningListener && listeningListener(port);
   })
   return server
