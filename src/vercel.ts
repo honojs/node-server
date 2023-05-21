d66be57f9b0882f16f0acceaac7087371e8f6cda
Yusuke Wada <yusuke@cloudflare.com>
2023-05-21 20:55:41 +0900

    fix(vercel): remove `handle` type error

--- src/vercel.ts
+++ src/vercel.ts
@@ -1,8 +1,6 @@
-import type { Env, Hono } from 'hono'
+import type { Hono } from 'hono'
 import { getRequestListener } from './listener'
 
-export const handle = <E extends Env, S extends {}, BasePath extends string>(
-  app: Hono<E, S, BasePath>
-) => {
+export const handle = (app: Hono<any, any, any>) => {
   return getRequestListener(app.fetch)
 }
