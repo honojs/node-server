ba4ece7f5541673698f5af8e918eb5bba729efc1
Taku Amano <taku@taaas.jp>
2023-11-15 21:17:51 +0900

    perf: Set properties directly instead of using Object.assign

--- src/listener.ts
+++ src/listener.ts
@@ -41,12 +41,10 @@ const responsePrototype: Record<string, any> = {
 })
 
 function newResponse(this: Response, body: BodyInit | null, init?: ResponseInit) {
-  Object.assign(this, {
-    status: init?.status || 200,
-    __body: body,
-    __init: init,
-    __cache: [body, (init?.headers || {}) as Record<string, string>],
-  })
+  ;(this as any).status = init?.status || 200
+  ;(this as any).__body = body
+  ;(this as any).__init = init
+  ;(this as any).__cache = [body, (init?.headers || {}) as Record<string, string>]
   if (typeof body !== 'string') {
     delete (this as any).__cache
   }
