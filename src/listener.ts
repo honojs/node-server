1e45efc48c92d7977de2b2120a0b8a099b8c0820
Taku Amano <taku@taaas.jp>
2023-11-17 21:33:59 +0900

    refactor: Cache only if `bodyInit` is a string or a ReadableStream

--- src/listener.ts
+++ src/listener.ts
@@ -44,7 +44,9 @@ function newResponse(this: Response, body: BodyInit | null, init?: ResponseInit)
   ;(this as any).status = init?.status || 200
   ;(this as any).__body = body
   ;(this as any).__init = init
-  ;(this as any).__cache = [body, (init?.headers || {}) as Record<string, string>]
+  if (typeof body === 'string' || body instanceof ReadableStream) {
+    ;(this as any).__cache = [body, (init?.headers || {}) as Record<string, string>]
+  }
 }
 newResponse.prototype = responsePrototype
 Object.defineProperty(global, 'Response', {
