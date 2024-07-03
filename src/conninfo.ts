b8fb04b72ce5449c8e492d9a7c3d679f7b75e98f
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2024-07-03 10:26:23 +0900

    feat: implement ConnInfo helper (#180)
    * feat: implement ConnInfo helper
    
    * chore: change package.json to export ConnInfo helper
    
    * fix: lint
    
    * remove `bun.lockb`
    
    ---------
    
    Co-authored-by: Yusuke Wada <yusuke@kamawada.com>

---
+++ src/conninfo.ts
@@ -0,0 +1,29 @@
+import type { AddressInfo } from 'net'
+import type { GetConnInfo } from 'hono/conninfo'
+import type { HttpBindings } from './types'
+
+/**
+ * ConnInfo Helper for Node.js
+ * @param c Context
+ * @returns ConnInfo
+ */
+export const getConnInfo: GetConnInfo = (c) => {
+  const bindings = (c.env.server ? c.env.server : c.env) as HttpBindings
+
+  const address = bindings.incoming.socket.address() as AddressInfo
+
+  if (!('address' in address)) {
+    return {
+      remote: {},
+    }
+  }
+
+  return {
+    remote: {
+      address: address.address,
+      addressType:
+        address.family === 'IPv4' ? 'IPv4' : address.family === 'IPv6' ? 'IPv6' : 'unknown',
+      port: address.port,
+    },
+  }
+}
