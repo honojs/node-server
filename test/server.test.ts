3c1a64729378823de828e911c8a1dd0c851ce611
Taku Amano <taku@taaas.jp>
2025-07-27 12:23:42 +0900

    feat: always respond `res.body` (#262)

--- test/server.test.ts
+++ test/server.test.ts
@@ -11,7 +11,7 @@ import { createServer as createHTTPSServer } from 'node:https'
 import { GlobalRequest, Request as LightweightRequest, getAbortController } from '../src/request'
 import { GlobalResponse, Response as LightweightResponse } from '../src/response'
 import { createAdaptorServer, serve } from '../src/server'
-import type { HttpBindings } from '../src/types'
+import type { HttpBindings, ServerType } from '../src/types'
 import { app } from './app'
 
 describe('Basic', () => {
@@ -133,126 +133,153 @@ describe('Basic', () => {
 })
 
 describe('various response body types', () => {
-  const app = new Hono()
-  app.use('*', async (c, next) => {
-    await next()
+  const runner = (Response: typeof GlobalResponse) => {
+    const largeText = 'a'.repeat(1024 * 1024 * 10)
+    let server: ServerType
+    let resolveReadableStreamPromise: () => void
+    beforeAll(() => {
+      const app = new Hono()
+      app.use('*', async (c, next) => {
+        await next()
 
-    // generate internal response object
-    const status = c.res.status
-    if (status > 999) {
-      c.res = new Response('Internal Server Error', { status: 500 })
-    }
-  })
-  app.get('/', () => {
-    const response = new Response('Hello! Node!')
-    return response
-  })
-  app.get('/uint8array', () => {
-    const response = new Response(new Uint8Array([1, 2, 3]), {
-      headers: { 'content-type': 'application/octet-stream' },
-    })
-    return response
-  })
-  app.get('/blob', () => {
-    const response = new Response(new Blob([new Uint8Array([1, 2, 3])]), {
-      headers: { 'content-type': 'application/octet-stream' },
-    })
-    return response
-  })
-  let resolveReadableStreamPromise: () => void
-  const readableStreamPromise = new Promise<void>((resolve) => {
-    resolveReadableStreamPromise = resolve
-  })
-  app.get('/readable-stream', () => {
-    const stream = new ReadableStream({
-      async start(controller) {
-        await readableStreamPromise
-        controller.enqueue('Hello!')
-        controller.enqueue(' Node!')
-        controller.close()
-      },
-    })
-    return new Response(stream)
-  })
-  app.get('/buffer', () => {
-    const response = new Response(Buffer.from('Hello Hono!'), {
-      headers: { 'content-type': 'text/plain' },
-    })
-    return response
-  })
-
-  app.use('/etag/*', etag())
-  app.get('/etag/buffer', () => {
-    const response = new Response(Buffer.from('Hello Hono!'), {
-      headers: { 'content-type': 'text/plain' },
-    })
-    return response
-  })
-
-  const server = createAdaptorServer(app)
-
-  it('Should return 200 response - GET /', async () => {
-    const res = await request(server).get('/')
-    expect(res.status).toBe(200)
-    expect(res.headers['content-type']).toMatch('text/plain')
-    expect(res.headers['content-length']).toMatch('12')
-    expect(res.text).toBe('Hello! Node!')
-  })
-
-  it('Should return 200 response - GET /uint8array', async () => {
-    const res = await request(server).get('/uint8array')
-    expect(res.status).toBe(200)
-    expect(res.headers['content-type']).toMatch('application/octet-stream')
-    expect(res.headers['content-length']).toMatch('3')
-    expect(res.body).toEqual(Buffer.from([1, 2, 3]))
-  })
-
-  it('Should return 200 response - GET /blob', async () => {
-    const res = await request(server).get('/blob')
-    expect(res.status).toBe(200)
-    expect(res.headers['content-type']).toMatch('application/octet-stream')
-    expect(res.headers['content-length']).toMatch('3')
-    expect(res.body).toEqual(Buffer.from([1, 2, 3]))
-  })
-
-  it('Should return 200 response - GET /readable-stream', async () => {
-    const expectedChunks = ['Hello!', ' Node!']
-    const resPromise = request(server)
-      .get('/readable-stream')
-      .parse((res, fn) => {
-        // response header should be sent before sending data.
-        expect(res.headers['transfer-encoding']).toBe('chunked')
-        resolveReadableStreamPromise()
-
-        res.on('data', (chunk) => {
-          const str = chunk.toString()
-          expect(str).toBe(expectedChunks.shift())
-        })
-        res.on('end', () => fn(null, ''))
+        // generate internal response object
+        const status = c.res.status
+        if (status > 999) {
+          c.res = new Response('Internal Server Error', { status: 500 })
+        }
       })
-    await new Promise((resolve) => setTimeout(resolve, 100))
-    const res = await resPromise
-    expect(res.status).toBe(200)
-    expect(res.headers['content-type']).toMatch('text/plain; charset=UTF-8')
-    expect(res.headers['content-length']).toBeUndefined()
-    expect(expectedChunks.length).toBe(0) // all chunks are received
+      app.get('/', () => {
+        const response = new Response('Hello! Node!')
+        return response
+      })
+      app.get('/large', () => {
+        // 10MB text
+        const response = new Response(largeText)
+        return response
+      })
+      app.get('/uint8array', () => {
+        const response = new Response(new Uint8Array([1, 2, 3]), {
+          headers: { 'content-type': 'application/octet-stream' },
+        })
+        return response
+      })
+      app.get('/blob', () => {
+        const response = new Response(new Blob([new Uint8Array([1, 2, 3])]), {
+          headers: { 'content-type': 'application/octet-stream' },
+        })
+        return response
+      })
+      const readableStreamPromise = new Promise<void>((resolve) => {
+        resolveReadableStreamPromise = resolve
+      })
+      app.get('/readable-stream', () => {
+        const stream = new ReadableStream({
+          async start(controller) {
+            await readableStreamPromise
+            controller.enqueue('Hello!')
+            controller.enqueue(' Node!')
+            controller.close()
+          },
+        })
+        return new Response(stream)
+      })
+      app.get('/buffer', () => {
+        const response = new Response(Buffer.from('Hello Hono!'), {
+          headers: { 'content-type': 'text/plain' },
+        })
+        return response
+      })
+
+      app.use('/etag/*', etag())
+      app.get('/etag/buffer', () => {
+        const response = new Response(Buffer.from('Hello Hono!'), {
+          headers: { 'content-type': 'text/plain' },
+        })
+        return response
+      })
+
+      server = createAdaptorServer(app)
+    })
+
+    it('Should return 200 response - GET /', async () => {
+      const res = await request(server).get('/')
+      expect(res.status).toBe(200)
+      expect(res.headers['content-type']).toMatch('text/plain')
+      expect(res.headers['content-length']).toMatch('12')
+      expect(res.text).toBe('Hello! Node!')
+    })
+
+    it('Should return 200 response - GET /large', async () => {
+      const res = await request(server).get('/large')
+      expect(res.status).toBe(200)
+      expect(res.headers['content-type']).toMatch('text/plain')
+      expect(res.headers['content-length']).toMatch(largeText.length.toString())
+      expect(res.text).toBe(largeText)
+    })
+
+    it('Should return 200 response - GET /uint8array', async () => {
+      const res = await request(server).get('/uint8array')
+      expect(res.status).toBe(200)
+      expect(res.headers['content-type']).toMatch('application/octet-stream')
+      expect(res.headers['content-length']).toMatch('3')
+      expect(res.body).toEqual(Buffer.from([1, 2, 3]))
+    })
+
+    it('Should return 200 response - GET /blob', async () => {
+      const res = await request(server).get('/blob')
+      expect(res.status).toBe(200)
+      expect(res.headers['content-type']).toMatch('application/octet-stream')
+      expect(res.headers['content-length']).toMatch('3')
+      expect(res.body).toEqual(Buffer.from([1, 2, 3]))
+    })
+
+    it('Should return 200 response - GET /readable-stream', async () => {
+      const expectedChunks = ['Hello!', ' Node!']
+      const resPromise = request(server)
+        .get('/readable-stream')
+        .parse((res, fn) => {
+          // response header should be sent before sending data.
+          expect(res.headers['transfer-encoding']).toBe('chunked')
+          resolveReadableStreamPromise()
+
+          res.on('data', (chunk) => {
+            const str = chunk.toString()
+            expect(str).toBe(expectedChunks.shift())
+          })
+          res.on('end', () => fn(null, ''))
+        })
+      await new Promise((resolve) => setTimeout(resolve, 100))
+      const res = await resPromise
+      expect(res.status).toBe(200)
+      expect(res.headers['content-type']).toMatch('text/plain; charset=UTF-8')
+      expect(res.headers['content-length']).toBeUndefined()
+      expect(expectedChunks.length).toBe(0) // all chunks are received
+    })
+
+    it('Should return 200 response - GET /buffer', async () => {
+      const res = await request(server).get('/buffer')
+      expect(res.status).toBe(200)
+      expect(res.headers['content-type']).toMatch('text/plain')
+      expect(res.headers['content-length']).toMatch('11')
+      expect(res.text).toBe('Hello Hono!')
+    })
+
+    it('Should return 200 response - GET /etag/buffer', async () => {
+      const res = await request(server).get('/etag/buffer')
+      expect(res.status).toBe(200)
+      expect(res.headers['content-type']).toMatch('text/plain')
+      expect(res.headers['etag']).toMatch('"7e03b9b8ed6156932691d111c81c34c3c02912f9"')
+      expect(res.headers['content-length']).toMatch('11')
+      expect(res.text).toBe('Hello Hono!')
+    })
+  }
+
+  describe('GlobalResponse', () => {
+    runner(GlobalResponse)
   })
 
-  it('Should return 200 response - GET /buffer', async () => {
-    const res = await request(server).get('/buffer')
-    expect(res.status).toBe(200)
-    expect(res.headers['content-type']).toMatch('text/plain')
-    expect(res.headers['content-length']).toMatch('11')
-    expect(res.text).toBe('Hello Hono!')
-  })
-
-  it('Should return 200 response - GET /etag/buffer', async () => {
-    const res = await request(server).get('/etag/buffer')
-    expect(res.status).toBe(200)
-    expect(res.headers['content-type']).toMatch('text/plain')
-    expect(res.headers['etag']).toMatch('"7e03b9b8ed6156932691d111c81c34c3c02912f9"')
-    expect(res.headers['content-length']).toMatch('11')
-    expect(res.text).toBe('Hello Hono!')
+  describe('LightweightResponse', () => {
+    runner(LightweightResponse as unknown as typeof GlobalResponse)
   })
 })
 
