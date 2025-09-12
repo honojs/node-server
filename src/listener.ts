6398480a4a8bba8a73091de65dc32642857ff619
K <kay-is@users.noreply.github.com>
2025-09-12 13:52:01 +0200

    Refactor promise check for response handling
    I got an error because `instanceof` wouldn't recognize `res` as a `Promise`. 
    
    Might be related to my usage of `tsx`, idk.
    
    Anyway, if I change to check for a `then` method, everything works again.

--- src/listener.ts
+++ src/listener.ts
@@ -103,7 +103,7 @@ const responseViaResponseObject = async (
   outgoing: ServerResponse | Http2ServerResponse,
   options: { errorHandler?: CustomErrorHandler } = {}
 ) => {
-  if (res instanceof Promise) {
+  if (typeof res.then === 'function') {
     if (options.errorHandler) {
       try {
         res = await res
