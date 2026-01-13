2d729e7c242e96d59df05aff6a54eb5e26148d12
Taku Amano <taku@taaas.jp>
2026-01-14 06:41:29 +0900

    fix(globals): Stop overwriting global.fetch (#295)

--- src/globals.ts
+++ src/globals.ts
@@ -1,19 +1,6 @@
 import crypto from 'node:crypto'
 
-const webFetch = global.fetch
-
 /** jest dose not use crypto in the global, but this is OK for node 18 */
 if (typeof global.crypto === 'undefined') {
   global.crypto = crypto as Crypto
-}
-
-global.fetch = (info, init?) => {
-  init = {
-    // Disable compression handling so people can return the result of a fetch
-    // directly in the loader without messing with the Content-Encoding header.
-    compress: false,
-    ...init,
-  } as RequestInit
-
-  return webFetch(info as RequestInfo, init)
 }
