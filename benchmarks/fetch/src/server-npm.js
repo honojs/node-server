cc05c48d8eb34007d5d5ff3861ea554721387041
Yusuke Wada <yusuke@kamawada.com>
2026-02-15 23:37:46 +0900

    chore: add benchmark for comparing with npm and local (dev) (#305)

---
+++ benchmarks/fetch/src/server-npm.js
@@ -0,0 +1,14 @@
+import { serve } from '@hono/node-server'
+import app from './app.js'
+
+const port = 3000
+
+serve(
+  {
+    fetch: app.fetch,
+    port,
+  },
+  () => {
+    console.log(`Listening on http://localhost:${port}`)
+  }
+)
