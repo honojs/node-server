6fb90ed1f888256c53d29b4908ffbc977be36d30
BlankParticle <blankparticle@gmail.com>
2026-03-13 14:31:08 +0530

    fix: only build exported entrypoints

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
-  entry: ['./src/**/*.ts'],
+  entry: [
+    './src/index.ts',
+    './src/serve-static.ts',
+    './src/vercel.ts',
+    './src/conninfo.ts',
+    './src/utils/*.ts',
+  ],
   format: ['esm', 'cjs'],
   dts: true,
   sourcemap: false,
